f85a413830972c74fef89ee34e5b0f801019942e
Bhimesh Solanki <bhimesh.solanki94@gmail.com>
2026-04-26 14:58:51 +0530

    Configured environment variables

--- src/main/java/com/moodjournal/demo/controller/JournalController.java
+++ src/main/java/com/moodjournal/demo/controller/JournalController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/journal")
-@CrossOrigin(origins = "*") // Allow React frontend to call this
+@CrossOrigin(origins = "${app.cors.allowed-origins}")
 public class JournalController {
 
     private final JournalRepository repository;
